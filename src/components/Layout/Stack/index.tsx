486b3c84dc0354be7bf99daf8516372505a61523
vitaliiverdiiev <vitaliiverdiiev@gmail.com>
2024-04-02 19:45:17 +0300

    clone: example repo

---
+++ src/components/Layout/Stack/index.tsx
@@ -0,0 +1,10 @@
+import { Box, BoxProps } from "@/components";
+import { cn } from "@/utils";
+
+type StackProps = BoxProps;
+
+export const Stack = ({ className, ...props }: StackProps) => {
+  return (
+    <Box className={cn("flex flex-col items-start", className)} {...props} />
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-import colors from 'tailwindcss/colors';
+import colors from "tailwindcss/colors";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
